c79bc3e754e86dd6586b074400343fa5c309d1cc
Jonathan Ong <jko665_000@msn.com>
2023-07-08 15:56:28 -0400

    Create project_1.py
    project about string manipulation

---
+++ projects/project_1.py
@@ -0,0 +1,9 @@
+def main():
+    print('This project is about string manipulation')
+    print('=========================================\n')
+    user_name = input('What is your name: ')
+    print(f'Hello {user_name}!\n')
+    print('I will count the number of times that a specific letter occurs in a message.\n')
+    message = input('Please eneter a message: ')
+    letter = input('Which letter would you like to count the occurences of: ')
+    print(f'\n{user_name}, your message has {message.lower().count(letter)} {letter}\'s in it.')
