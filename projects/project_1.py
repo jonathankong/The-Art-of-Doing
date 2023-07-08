8be61257a93a53fff652f11ea1fb36ba00b88434
Jonathan Ong <jko665_000@msn.com>
2023-07-08 15:57:23 -0400

    Update project_1.py
    made sure message and letter input are lowercase before counting letters

--- projects/project_1.py
+++ projects/project_1.py
@@ -6,4 +6,4 @@ def main():
     print('I will count the number of times that a specific letter occurs in a message.\n')
     message = input('Please eneter a message: ')
     letter = input('Which letter would you like to count the occurences of: ')
-    print(f'\n{user_name}, your message has {message.lower().count(letter)} {letter}\'s in it.')
+    print(f'\n{user_name}, your message has {message.lower().count(letter.lower())} {letter}\'s in it.')
